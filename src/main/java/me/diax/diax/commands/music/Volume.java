17a5ddb7fddaf1a04695dc4ed3e17483d32d8ed7
comportment <l1k3asir>
2017-10-27 20:45:30 +0100

    volume changes

--- src/main/java/me/diax/diax/commands/music/Volume.java
+++ src/main/java/me/diax/diax/commands/music/Volume.java
@@ -1,5 +1,6 @@
 package me.diax.diax.commands.music;
 
+import com.sedmelluq.discord.lavaplayer.player.AudioPlayer;
 import me.diax.comportment.jdacommand.Command;
 import me.diax.comportment.jdacommand.CommandAttribute;
 import me.diax.comportment.jdacommand.CommandDescription;
@@ -14,19 +15,24 @@ import net.dv8tion.jda.core.entities.Message;
                 "vol"
         },
         attributes = @CommandAttribute(key = "patreon"),
-        description = "[0-150]"
+        description = "{0-150}"
 )
 public class Volume implements Command {
 
     @Override
     public void execute(Message trigger, String truncated) {
+        AudioPlayer player = GuildMusicManager.getManagerFor(trigger.getGuild()).getPlayer();
+        if (truncated.isEmpty()) {
+            trigger.getChannel().sendMessage(Emote.MUSICAL_NOTE + " - The volume is [" + player.getVolume() + "/150]").queue();
+            return;
+        }
         try {
             int volume = Integer.valueOf(truncated.split("\\s+")[0]);
             if (volume > 150 || volume < 0) throw new NumberFormatException("Invalid number in expected range.");
-            GuildMusicManager.getManagerFor(trigger.getGuild()).getPlayer().setVolume(volume);
-            trigger.getChannel().sendMessage(Emote.MUSICAL_NOTE + " - The volume has been set to: (" + volume + "/150)").queue();
+            player.setVolume(volume);
+            trigger.getChannel().sendMessage(Emote.MUSICAL_NOTE + " - The volume has been set to [" + volume + "/150]").queue();
         } catch (NumberFormatException e) {
-            trigger.getChannel().sendMessage(Emote.X + " - That is not a valid number (0-150)!").queue();
+            trigger.getChannel().sendMessage(Emote.X + " - That is not a valid number [0/150]!").queue();
         }
     }
 }
