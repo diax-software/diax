5956e90898a2a8b28a05ed2afc8c02348a74e31b
comportment <l1k3asir>
2017-10-28 13:11:56 +0100

    Better formatting of java version

--- src/main/java/me/diax/diax/commands/information/Info.java
+++ src/main/java/me/diax/diax/commands/information/Info.java
@@ -28,6 +28,7 @@ public class Info implements Command {
     public void execute(Message message, String s) {
         JDA jda = message.getJDA();
         Runtime runtime = Runtime.getRuntime();
+        String java = System.getProperty("java.runtime.version");
         message.getChannel().sendMessage(Embed.transparent().setDescription(String.join("\n",
                 "```prolog",
                 "----- Library Versions -----",
@@ -49,7 +50,7 @@ public class Info implements Command {
                 "----- VPS Information -----",
                 "",
                 "Location: " + System.getProperty("user.country"),
-                "Java Version: " + System.getProperty("java.runtime.version"),
+                "Java Version: " + (java.contains("-") ? java.split("-")[0] : java),
                 "OS: " + System.getProperty("os.name") + "-" + System.getProperty("os.version"),
                 "OS Arch: " + System.getProperty("os.arch"),
                 "RAM: " + ((runtime.totalMemory() - runtime.freeMemory()) / 1024 / 1024) + "Mb/" + (runtime.totalMemory() / 1024 / 1024) + "Mb",
